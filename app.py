a8c462e539546d796dbe37bc7f83d40eb13d779d
Rajesh <rajeshshedolkar12@gmail.com>
2018-08-28 17:51:15 +0530

    adding get profile details api

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask
 from flask import render_template
-
+import requests
+import json
 app = Flask(__name__)
-
+GITHUB_BASE_URL = "https://api.github.com"
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return "Welcome to Flask Application!!!"
+
+@app.route('/<username>')
+def profile(username):
+    url = GITHUB_BASE_URL+'/users/%s'%username
+    r = requests.get(url, auth=('shedolkar12', 'Shaddy@12'))
+    url = GITHUB_BASE_URL + '/users/%s/repos'%username
+    repos = requests.get(url, auth=('shedolkar12', 'Shaddy@12'))
+    content = r.json()
+    content['repos'] = []
+    for obj in repos.json():
+        d = dict()
+        d['name'] = obj['name']
+        d['url'] = obj['url']
+        d['date_created'] = obj['created_at']
+        content['repos'].append(d)
+    return render_template('profile.html', **content)
 
 @app.route('/<n>')
 def hello_name(n):
